d2c1d2e261d7deb61daa060cfdb2dc519fdd7c04
Ruben student <462480@student.fontys.nl>
2024-03-18 05:44:58 +0100

    Added Werewolf Descriptions

---
+++ ViewModels/CharacterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WeerwolvenOnline.ViewModels
+{
+    public class CharacterModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDead { get; set; }
+        public bool IsWerewolf { get; set; }
+        public bool IsVillager { get; set; }
+    }
+}
